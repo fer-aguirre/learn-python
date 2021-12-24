935baf6673d608cdfeac1e87037ffe17ffb14529
FXR <kay_op@protonmail.com>
2021-12-24 14:46:00 -0600

    Add new functions to hangman_game.py

--- handle_errors/hangman_game.py
+++ handle_errors/hangman_game.py
@@ -8,9 +8,23 @@ import os
 # Clean screen
 # os.system('clear')
 
-def hangman():
-    pass
+# class Hangman:
+#     def __init__(self, name):
+#         self.name = name
 
+def get_word():
+    list_words = open('./data/data.txt', 'r', encoding='utf-8').read().splitlines()
+    word = random.choice(list_words)
+    return word
+
+def hide_word(word):
+    hidden_word = '_' * len(word)
+    return print(hidden_word)
+
+def run():
+    word = get_word()
+    print(word)
+    hide_word(word)
 
 if __name__ == "__main__":
-    pass
+    run()
