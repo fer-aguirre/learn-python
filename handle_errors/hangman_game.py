77a75090e3805675e827496aa56e62a7c9dedfb1
FXR <kay_op@protonmail.com>
2021-12-27 22:56:05 -0600

    Create a new function to hangman.py

--- handle_errors/hangman_game.py
+++ handle_errors/hangman_game.py
@@ -4,27 +4,55 @@ Script to play hangman game
 
 import random
 import os
+from unidecode import unidecode
 
-# Clean screen
-# os.system('clear')
 
 # class Hangman:
 #     def __init__(self, name):
 #         self.name = name
 
 def get_word():
+    # Read a file by lines and create a list
     list_words = open('./data/data.txt', 'r', encoding='utf-8').read().splitlines()
-    word = random.choice(list_words)
+    # Remove accents, upper case and choose a random word from list
+    word = unidecode(random.choice(list_words)).lower()
     return word
 
 def hide_word(word):
     hidden_word = '_' * len(word)
-    return print(hidden_word)
+    hidden_word = [u for u in hidden_word]
+    return print(f'Tu palabra secreta tiene {len(hidden_word)} letras \n\n {hidden_word}\n')
+
+def guess_word(word):
+    guess = input('Escoge una letra: ')
+    assert guess.isalpha(), 'Sólo puedes ingresar letras'
+    # Command to clear terminal
+    os.system('clear')
+    # Create a list with word letters
+    letters = [l for l in word]
+    # Cover letters that doesn't match the input
+    for i in range(len(letters)):
+        if guess != letters[i]:
+            letters[i] = '_'
+    # Create a list with matches from input
+    count = list(filter(lambda l: l == guess, letters))
+     
+    if guess in letters:
+        if len(count) > 1:
+            print(f"Hay {len(count)} coincidencias de '{guess}'\n")
+        else:
+            print(f"Hay {len(count)} coincidencia de '{guess}'\n")
+    else:
+        print(f"Ups, '{guess}' no se encuentra en la palabra secreta :(\n")
+
+    return print(letters)
 
 def run():
     word = get_word()
     print(word)
     hide_word(word)
+
+    guess_word(word)
 
 if __name__ == "__main__":
     run()
