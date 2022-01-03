c2e8578edce526ce60ee84dffd2e16f5c182d817
FXR <kay_op@protonmail.com>
2022-01-03 00:21:45 -0600

    Decompose hangman game functions

--- handle_errors/hangman_game.py
+++ handle_errors/hangman_game.py
@@ -6,53 +6,75 @@ import random
 import os
 from unidecode import unidecode
 
-
-# class Hangman:
-#     def __init__(self, name):
-#         self.name = name
+# Constant with a file path
+DATA = "./data/data.txt"
 
 def get_word():
     # Read a file by lines and create a list
-    list_words = open('./data/data.txt', 'r', encoding='utf-8').read().splitlines()
+    list_words = open(DATA, 'r', encoding='utf-8').read().splitlines()
     # Remove accents, upper case and choose a random word from list
     word = unidecode(random.choice(list_words)).lower()
     return word
 
 def hide_word(word):
-    hidden_word = '_' * len(word)
-    hidden_word = [u for u in hidden_word]
-    return print(f'Tu palabra secreta tiene {len(hidden_word)} letras \n\n {hidden_word}\n')
+    hidden_word = '_ ' * len(word)
+    return hidden_word
 
-def guess_word(word):
-    guess = input('Escoge una letra: ')
-    assert guess.isalpha(), 'Sólo puedes ingresar letras'
-    # Command to clear terminal
-    os.system('clear')
-    # Create a list with word letters
-    letters = [l for l in word]
-    # Cover letters that doesn't match the input
-    for i in range(len(letters)):
-        if guess != letters[i]:
-            letters[i] = '_'
-    # Create a list with matches from input
-    count = list(filter(lambda l: l == guess, letters))
-     
-    if guess in letters:
-        if len(count) > 1:
-            print(f"Hay {len(count)} coincidencias de '{guess}'\n")
+def is_word_guessed():
+    pass
+
+def letters_chosen():
+    pass
+
+def count_attempts():
+    pass
+
+def uncover_word(word, letters_guessed):
+    uncovered = ''
+    for letter in word:
+        if letter in letters_guessed:
+           uncovered += letter
         else:
-            print(f"Hay {len(count)} coincidencia de '{guess}'\n")
-    else:
-        print(f"Ups, '{guess}' no se encuentra en la palabra secreta :(\n")
+            uncovered += '_'
+    return uncovered
 
-    return print(letters)
+#     # Command to clear terminal
+#     os.system('clear')
+
+#     # Create a list with matches from input
+#     count = list(filter(lambda l: l == guess, letters))
+
+def guess_letter(word):
+    while True:
+        letters = [l for l in word]
+        guess = input("Escoge una letra: ")
+        assert guess.isalpha(), "Sólo puedes escoger letras"
+        if guess not in word:
+           print(f"No hay coincidencias de '{guess}'")
+        else:
+            # Cover letters that doesn't match the input
+            for i in range(len(word)):
+                if guess != letters[i]:
+                    letters[i] = '_'
+            print(f"  {' '.join(letters)} ")
+            
+
+            if '_' not in letters:
+                break
+            else:
+                continue
+
+def hangman_game():
+    pass
 
 def run():
     word = get_word()
     print(word)
-    hide_word(word)
-
-    guess_word(word)
+    hidden_word = hide_word(word)
+    print(f'Tu palabra secreta tiene {len(word)} letras \n\n {hidden_word}\n')
+    guess = guess_letter(word)
 
 if __name__ == "__main__":
     run()
+
+     
