3094b3eee45aeae64eea822fb79ef2d7d3ae01d0
FXR <kay_op@protonmail.com>
2022-01-16 23:42:11 -0600

    Delete nltk directory in nlp

--- handle_errors/hangman_game.py
+++ handle_errors/hangman_game.py
@@ -4,45 +4,73 @@ Script to play hangman game
 
 import random
 import os
+from xml.dom import WrongDocumentErr
 from unidecode import unidecode
 
 # Constant with a file path
 DATA = "./data/hangman_data.txt"
 
+"""
+Function to read a txt file
+"""
 def get_word():
+    """
+    return: a random word from file
+    """
     # Read a file by lines and create a list
     list_words = open(DATA, 'r', encoding='utf-8').read().splitlines()
     # Remove accents, upper case and choose a random word from list
     word = unidecode(random.choice(list_words)).lower()
     return word
 
+"""
+Function to cover the
+word with underscores
+"""
 def hide_word(word):
+    """
+    word: random word generated from get_word() function
+    return: string with underscores
+    """
     hidden_word = '_ ' * len(word)
     return hidden_word
 
-def is_word_guessed():
-    pass
 
-def letters_chosen():
-    pass
+"""
+Function to check if a 
+letter is into the word
+"""
+def is_letter_in_word(letter, word):
+    """
+    letter: character from input
+    word: random word generated from get_word() function
+    return: boolean, True if letter is in word and False if it isn't
+    """
+    if letter in word:
+        return True
+    else:
+        return False
 
-def count_attempts():
-    pass
-
-def uncover_word(word, letters_guessed):
-    uncovered = ''
-    for letter in word:
-        if letter in letters_guessed:
-           uncovered += letter
+"""
+Function to uncover the word
+with matches from input
+"""
+def uncover_word(letter, word, hidden_word):
+    for char in word, hidden_word:
+        if char == letter:
+           hidden_word += char
         else:
-            uncovered += '_'
-    return uncovered
+            hidden_word += '_ '
+    return hidden_word
 
-#     # Command to clear terminal
-#     os.system('clear')
+"""
+Function to clear the terminal
+"""
+def clean_terminal():
+    # Command to clear terminal
+    os.system('clear')
 
-#     # Create a list with matches from input
-#     count = list(filter(lambda l: l == guess, letters))
+
 
 def guess_letter(word):
     while True:
@@ -65,14 +93,21 @@ def guess_letter(word):
                 continue
 
 def hangman_game():
-    pass
-
-def run():
     word = get_word()
     print(word)
     hidden_word = hide_word(word)
     print(f'Tu palabra secreta tiene {len(word)} letras \n\n {hidden_word}\n')
-    guess = guess_letter(word)
+    guess = input("Escoge una letra: ")
+    is_match = is_letter_in_word(guess, word)
+    if is_match == True:
+        uncovered = uncover_word(guess, word, hidden_word)
+        print(uncovered)
+    else:
+        pass
+
+def run():
+    hangman_game()
+
 
 if __name__ == "__main__":
     run()
